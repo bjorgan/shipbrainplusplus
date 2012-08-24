6b426f95ea4a0405f52fa384ca833dd89be078a5
Kjetil 'Snow' Pettersen <kp1085@nyu.edu>
2012-08-24 10:46:56 +0200

    hemmelig bare masse dritt segfaults og alt

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,16 @@
 #include "mainwindow.h"
 #include "ocean.h"
 #include <QWidget>
+#include <QMenu>
+#include <QMenuBar>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
 	Ocean *ocean = new Ocean(this);
 	setCentralWidget(ocean);
+	QAction * sstop = new QAction ("&Sstop", this);
+	QMenu * menu;
+	menu->addAction(sstop);
+	connect(sstop, SIGNAL(triggered()), this, SLOT(ocean->flipTimeState));
+	
+
 }
