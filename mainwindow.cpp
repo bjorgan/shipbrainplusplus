b7d975eb6fc5c1ca582736abae6e5bfa4a95525c
Kjetil 'Snow' Pettersen <kp1085@nyu.edu>
2012-08-24 17:22:36 +0200

    rensket kode og COMMENTS

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,18 +2,13 @@
 #include "ocean.h"
 #include <QWidget>
 #include <QToolBar>
-#include <QIcon>
 #include <QAction>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
 	Ocean *ocean = new Ocean(this);
 	setCentralWidget(ocean);
-	QPixmap sstoppix("new.png");
+	// Defines the toolbar for the wave;
 	QToolBar *toolbar = addToolBar("main tool bar");
-	QAction *sstop = toolbar->addAction(QIcon(sstoppix), "Start/stop wave");
+	QAction *sstop = toolbar->addAction("Start/stop wave");
 	connect(sstop, SIGNAL(triggered()),ocean , SLOT(flipTimeState()));
-
-
-	
-
 }
