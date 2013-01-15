7ac98d0433a5c81b61620c5e696f6cce656f89a1
Kjetil Pettersen <snow@red.(none)>
2013-01-15 11:17:15 -0500

    size of window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,10 @@
 #include "mainwindow.h"
 #include "ocean.h"
 #include <QWidget>
+#include <QSize>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
 	Ocean *ocean = new Ocean(this);
 	setCentralWidget(ocean);
+	resize(width(), height());
 }
